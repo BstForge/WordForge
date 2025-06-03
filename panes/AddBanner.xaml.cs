1338ac3060f1b0757aa969e9bae1de6f72d8bdf8
BstForge <archimage2869@gmail.com>
2025-06-03 16:30:47 -0500

    Phase 4.6 - Bottom Banner

---
+++ panes/AddBanner.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WordForge.Panes
+{
+    public partial class AdBanner : UserControl
+    {
+        public AdBanner()
+        {
+            InitializeComponent();
+        }
+    }
+}
